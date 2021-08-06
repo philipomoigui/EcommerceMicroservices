ab272680b4b3439d5464d7afd742430bcfe526a1
Philip Omoigui <philipomoigui@gmail.com>
2021-08-06 16:23:58 +0100

    Added repository pattern to basket APIservice

---
+++ src/Services/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in ShoppingCartItems)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+
+
+    }
+}
---
+++ src/Services/Basket.API/Repositories/BasketRespository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRespository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRespository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.Username);
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+           await _redisCache.RemoveAsync(username);
+        }
+    }
+}
