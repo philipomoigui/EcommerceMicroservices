f2fc2cbc5d21c8009a49e53298abac964ba76707
philip omoigui <philip.omoigui@innotelligent.com>
2021-08-23 00:23:44 +0100

    Dockerized order api and ms sql database

--- src/Services/Order/Ordering.API/Extensions/HostExtension.cs
+++ src/Services/Order/Ordering.API/Extensions/HostExtension.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System;
+
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API.Extensions
 {
--- src/Services/Order/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Services/Order/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -1,18 +1,23 @@
-﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using MediatR;
+
 
 namespace Ordering.Application.Behaviours
 {
     public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
         private readonly ILogger<TRequest> _logger;
-     
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             try
