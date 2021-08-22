16e2fba9bd89ef56f7d336a3d3f2e121fc3c95b0
philip omoigui <philip.omoigui@innotelligent.com>
2021-08-22 14:29:09 +0100

    Added implementation for application infrastructure layer

---
+++ src/Services/Order/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using Ordering.Application.Contracts.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == HttpStatusCode.Accepted || response.StatusCode == HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending failed.");
+            return false;
+        }
+    }
+}
